6fd1aced17cba9980bce698fece69e66f241fe7d
randomlshb@gmail.com <randomlshb@gmail.com>
2019-11-16 13:14:00 -0600

    Some comments

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,22 @@ extern crate lazy_static;
 mod json;
 use json::*;
 
-pub const MAX_REPEAT: usize = 1000;
+pub const MAX_REPEAT: usize = 1024;
 pub const MAX_STEPS: u64 = 256000;
 
 fn main() {
     let mut res = HashSet::new();
     loop {
+        // Print status
         if res.len() % 100000 == 0 {
             print!("Generated: {}\n", res.len());
         }
+
+        // Generate and attempt to add the testcase to a HashSet,
+        // which will implicitly dedup for us
         res.insert(Json::default().value);
+
+        // If we hit our maximum of 1M test cases, break
         if res.len() >= 1000000 {
             break 
         }
